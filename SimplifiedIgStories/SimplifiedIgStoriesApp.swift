fb59feba6ec65f30799e7d5ac29816c9b784197e
tzc1234 <peopli9997@gmail.com>
2022-04-26 13:51:32 +0100

    Setup Cashlytics.

--- SimplifiedIgStories/SimplifiedIgStoriesApp.swift
+++ SimplifiedIgStories/SimplifiedIgStoriesApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import Firebase
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct SimplifiedIgStoriesApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
