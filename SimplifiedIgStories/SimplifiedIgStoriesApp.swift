f06d6e6b721dd3faba8e49d3b0646bf82e4a854d
tzc1234 <peopli9997@gmail.com>
2022-04-26 18:21:52 +0100

    Setup DEV and TEST environments.

--- SimplifiedIgStories/SimplifiedIgStoriesApp.swift
+++ SimplifiedIgStories/SimplifiedIgStoriesApp.swift
@@ -12,7 +12,13 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions
                      launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
+        #if DEV
+        print("DEV environment now.")
+        #elseif TEST
+        print("TEST environment now.")
         FirebaseApp.configure()
+        #endif
+        
         return true
     }
 }
