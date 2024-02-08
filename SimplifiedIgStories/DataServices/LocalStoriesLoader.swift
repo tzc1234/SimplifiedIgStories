eb93b9e124d2392b4b51730e334360386ee9b991
tzc1234 <peopli9997@gmail.com>
2024-02-08 13:56:39 +0000

    Extract LocalStory mapping logic to LocalStoriesMapper

--- SimplifiedIgStories/DataServices/LocalStoriesLoader.swift
+++ SimplifiedIgStories/DataServices/LocalStoriesLoader.swift
@@ -19,58 +19,15 @@ final class LocalStoriesLoader {
         case invalidData
     }
     
-    private struct DecodableStory: Decodable {
-        let id: Int
-        let lastUpdate: TimeInterval?
-        let user: User
-        let portions: [Portion]
-        
-        var local: LocalStory {
-            .init(
-                id: id,
-                lastUpdate: lastUpdate.map(Date.init(timeIntervalSince1970:)),
-                user: user.local,
-                portions: portions.map(\.local)
-            )
-        }
-        
-        struct User: Decodable {
-            let id: Int
-            let name: String
-            let avatar: String
-            let isCurrentUser: Bool
-            
-            var local: LocalUser {
-                .init(id: id, name: name, avatar: avatar, isCurrentUser: isCurrentUser)
-            }
-        }
-        
-        struct Portion: Decodable {
-            let id: Int
-            let resource: String
-            let duration: Double?
-            let type: String
-            
-            var local: LocalPortion {
-                .init(
-                    id: id,
-                    resource: resource,
-                    duration: duration ?? .defaultStoryDuration,
-                    type: .init(rawValue: type) ?? .image
-                )
-            }
-        }
-    }
-
     func load() async throws -> [LocalStory] {
         guard let data = try? await client.fetch() else {
             throw Error.notFound
         }
         
-        guard let stories = try? JSONDecoder().decode([DecodableStory].self, from: data) else {
+        guard let stories = try? LocalStoriesMapper.map(data) else {
             throw Error.invalidData
         }
         
-        return stories.map(\.local)
+        return stories
     }
 }
