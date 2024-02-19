521cf5f65161478efbd381f55f4c8764568492e1
tzc1234 <peopli9997@gmail.com>
2024-02-19 12:19:15 +0000

    Add makeSUT to reduce redundant code

--- SimpleIGStoryTests/Camera/AVPhotoTakerTests.swift
+++ SimpleIGStoryTests/Camera/AVPhotoTakerTests.swift
@@ -11,8 +11,7 @@ import AVKit
 
 final class AVPhotoTakerTests: XCTestCase {
     func test_init_doesNotDeliverStatusUponInit() {
-        let device = PhotoCaptureDeviceSpy(isSessionRunning: false, performOnSessionQueue: { _ in })
-        let sut = AVPhotoTaker(device: device)
+        let (sut, device) = makeSUT()
         let statusSpy = StatusSpy<PhotoTakerStatus>(publisher: sut.getStatusPublisher())
         
         XCTAssertTrue(statusSpy.loggedStatuses.isEmpty)
@@ -20,38 +19,44 @@ final class AVPhotoTakerTests: XCTestCase {
     
     func test_takePhoto_addsPhotoOutputToSessionIfNoPhotoOutputWhenSessionIsNotRunning() throws {
         let exp = expectation(description: "Wait for session queue")
-        let device = PhotoCaptureDeviceSpy(isSessionRunning: false, performOnSessionQueue: { action in
-            action()
-            exp.fulfill()
-        })
-        let sut = AVPhotoTaker(device: device)
+        let (sut, device) = makeSUT(perform: { exp.fulfill() })
         
         sut.takePhoto(on: .off)
         wait(for: [exp], timeout: 1)
         
-        XCTAssertEqual(device.sessionSpy?.loggedPhotoOutputs.count, 1)
+        XCTAssertEqual(device.sessionSpy.loggedPhotoOutputs.count, 1)
     }
     
     func test_takePhoto_addsPhotoOutputToSessionIfNoPhotoOutputWhenSessionIsRunning() throws {
         let exp = expectation(description: "Wait for session queue")
-        let device = PhotoCaptureDeviceSpy(isSessionRunning: true, performOnSessionQueue: { action in
-            action()
-            exp.fulfill()
-        })
-        let sut = AVPhotoTaker(device: device)
+        let (sut, device) = makeSUT(perform: { exp.fulfill() })
         
         sut.takePhoto(on: .off)
         wait(for: [exp], timeout: 1)
         
-        XCTAssertEqual(device.sessionSpy?.loggedPhotoOutputs.count, 1)
+        XCTAssertEqual(device.sessionSpy.loggedPhotoOutputs.count, 1)
     }
     
     // MARK: - Helpers
     
+    private func makeSUT(isSessionRunning: Bool = false,
+                         perform: @escaping () -> Void = {},
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: AVPhotoTaker, device: PhotoCaptureDeviceSpy) {
+        let device = PhotoCaptureDeviceSpy(isSessionRunning: isSessionRunning, performOnSessionQueue: { action in
+            action()
+            perform()
+        })
+        let sut = AVPhotoTaker(device: device)
+        trackForMemoryLeaks(device, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, device)
+    }
+    
     private final class PhotoCaptureDeviceSpy: PhotoCaptureDevice {
         let session: AVCaptureSession
-        var sessionSpy: CaptureSessionSpy? {
-            session as? CaptureSessionSpy
+        var sessionSpy: CaptureSessionSpy {
+            session as! CaptureSessionSpy
         }
         
         private(set) var cameraPosition: CameraPosition = .back
