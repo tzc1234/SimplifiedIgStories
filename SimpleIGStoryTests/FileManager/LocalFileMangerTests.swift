aef3c032d1f86501fb9efa7065b538ecd2b13018
tzc1234 <peopli9997@gmail.com>
2024-03-12 09:58:22 +0000

    LocalFileManager.getImage twice ensures no side effects when no image

--- SimpleIGStoryTests/FileManager/LocalFileMangerTests.swift
+++ SimpleIGStoryTests/FileManager/LocalFileMangerTests.swift
@@ -9,12 +9,28 @@ import XCTest
 @testable import Simple_IG_Story
 
 final class LocalFileMangerTests: XCTestCase {
-    func test_getImage_doesNotDeliverImageWhenNoImageFound() {
+    func test_getImage_doesNotDeliverImageWhenNoImage() {
         let sut = LocalFileManager()
-        let url = FileManager.default.temporaryDirectory.appendingPathComponent("test.jpg")
         
-        let image = sut.getImage(for: url)
+        let image = sut.getImage(for: imageFileURL())
         
         XCTAssertNil(image)
+    }
+    
+    func test_getImageTwice_ensuresNoSideEffectsWhenNoImage() {
+        let sut = LocalFileManager()
+        let url = imageFileURL()
+        
+        let firstImage = sut.getImage(for: url)
+        let lastImage = sut.getImage(for: url)
+        
+        XCTAssertNil(firstImage)
+        XCTAssertNil(lastImage)
+    }
+    
+    // MARK: - Helpers
+    
+    private func imageFileURL() -> URL {
+        FileManager.default.temporaryDirectory.appendingPathComponent("test.jpg")
     }
 }
