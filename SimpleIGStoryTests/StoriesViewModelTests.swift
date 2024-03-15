372fdf0410916fcf1c86730bad945a5b564c13e2
tzc1234 <peopli9997@gmail.com>
2024-03-15 14:12:32 +0000

    Update StoriesViewModelTests

--- SimpleIGStoryTests/StoriesViewModelTests.swift
+++ SimpleIGStoryTests/StoriesViewModelTests.swift
@@ -9,52 +9,57 @@ import XCTest
 @testable import Simple_IG_Story
 
 class StoriesViewModelTests: XCTestCase {
-    
-    var sut: StoriesViewModel!
-    
-    override func setUpWithError() throws {
-        sut = StoriesViewModel(fileManager: LocalFileManager())
+    func test_fetchStories_ensuresOneCurrentUserInStories() async {
+        let sut = makeSUT()
         
-        let expectation = XCTestExpectation(description: "wait async fetchStories")
+        await sut.fetchStories()
         
-        Task {
-            await sut.fetchStories()
-            expectation.fulfill()
-        }
-        
-        wait(for: [expectation], timeout: 0.5)
-    }
-
-    override func tearDownWithError() throws {
-        sut = nil
-    }
-
-    func test_stories_ensureOneCurrentUserInStories() {
-        let stories = sut.stories
-        let currentUserStory = stories.first(where: { $0.user.isCurrentUser })
-        let currentUserStoryIdx = stories.firstIndex(where: { $0.user.isCurrentUser })
-        
-        XCTAssertNotNil(currentUserStory, "currentUserStory")
-        XCTAssertNotNil(currentUserStoryIdx, "currentUserStoryIdx")
+        let currentUserStories = sut.stories.filter { $0.user.isCurrentUser }
+        XCTAssertEqual(currentUserStories.count, 1)
     }
     
-    func test_currentStories_shouldContainOnlyOneCurrentUserStory_whenCurrentStoryIdIsSetToCurrentUserStoryId() throws {
-        let currentUserStoryId = try XCTUnwrap(sut.stories.first(where: { $0.user.isCurrentUser })?.id)
+    func test_currentStories_containsOnlyOneCurrentUserStoryWhenCurrentStoryIdIsCurrentUserStoryId() async throws {
+        let sut = makeSUT()
+        await sut.fetchStories()
         
+        let currentUserStoryId = try XCTUnwrap(sut.stories.first { $0.user.isCurrentUser }?.id)
         sut.setCurrentStoryId(currentUserStoryId)
         
-        XCTAssertEqual(sut.currentStoryId, currentUserStoryId, "currentStoryId == currentUserStoryId")
-        XCTAssertEqual(sut.currentStories.count, 1, "currentStories.count == 1")
-        XCTAssertEqual(sut.currentStories.first?.id, currentUserStoryId, "currentStories.first.id == currentUserStoryId")
+        XCTAssertEqual(sut.currentStoryId, currentUserStoryId)
+        XCTAssertEqual(sut.currentStories.count, 1)
+        XCTAssertEqual(sut.currentStories.first?.id, currentUserStoryId)
     }
     
-    func test_currentStories_shouldNotContainCurrentUserStory_whenCurrentStoryIdIsNotCurrentUserStoryId() throws {
-        let nonCurrentUserStoryId = try XCTUnwrap(sut.stories.first(where: { !$0.user.isCurrentUser })?.id)
+    func test_currentStories_containNoCurrentUserStoryWhenCurrentStoryIdIsNonCurrentUserStoryId() async throws {
+        let sut = makeSUT()
+        await sut.fetchStories()
         
+        let nonCurrentUserStoryId = try XCTUnwrap(sut.stories.first { !$0.user.isCurrentUser }?.id)
         sut.setCurrentStoryId(nonCurrentUserStoryId)
         
-        XCTAssertEqual(sut.currentStoryId, nonCurrentUserStoryId, "currentStoryId == nonCurrentUserStoryId")
-        XCTAssertEqual(sut.currentStories.filter { $0.user.isCurrentUser }.count, 0, "currentStories")
+        XCTAssertEqual(sut.currentStoryId, nonCurrentUserStoryId)
+        let currentUserStoriesInCurrentStories = sut.currentStories.filter { $0.user.isCurrentUser }
+        XCTAssertEqual(currentUserStoriesInCurrentStories.count, 0)
     }
     
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath,
+                         line: UInt = #line) -> StoriesViewModel {
+        let sut = StoriesViewModel(fileManager: DummyFileManager())
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private class DummyFileManager: FileManageable {
+        func saveImage(_ image: UIImage, fileName: String) throws -> URL {
+            URL(string: "file://any-image.jpg")!
+        }
+        
+        func getImage(for url: URL) -> UIImage? {
+            nil
+        }
+        
+        func delete(for url: URL) throws {}
+    }
 }
