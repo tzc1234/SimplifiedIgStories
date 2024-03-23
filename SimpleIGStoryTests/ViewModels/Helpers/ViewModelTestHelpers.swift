8692fe7308c621d68c8b41b7e50f31a39f69b1ae
tzc1234 <peopli9997@gmail.com>
2024-03-23 16:41:08 +0000

    StoryPortionViewModel.deletePortionMedia ignores when no portion resourceURL

--- SimpleIGStoryTests/ViewModels/Helpers/ViewModelTestHelpers.swift
+++ SimpleIGStoryTests/ViewModels/Helpers/ViewModelTestHelpers.swift
@@ -8,8 +8,8 @@
 import Foundation
 @testable import Simple_IG_Story
 
-func makePortion(id: Int = 0) -> Portion {
-    Portion(id: id, duration: 1, resourceURL: nil, type: .image)
+func makePortion(id: Int = 0, resourceURL: URL? = nil) -> Portion {
+    Portion(id: id, duration: 1, resourceURL: resourceURL, type: .image)
 }
 
 func makeStory(id: Int = 0, portions: [Portion] = [], isCurrentUser: Bool = false) -> Story {
---
+++ SimpleIGStoryTests/ViewModels/StoryPortionViewModelTests.swift
@@ -0,0 +1,60 @@
+//
+//  StoryPortionViewModelTests.swift
+//  SimpleIGStoryTests
+//
+//  Created by Tsz-Lung on 23/03/2024.
+//
+
+import XCTest
+@testable import Simple_IG_Story
+
+final class StoryPortionViewModelTests: XCTestCase {
+    func test_deletePortionMedia_ignoresWhenNoPortionResourceURL() {
+        let fileManager = FileManagerSpy()
+        let sut = makeSUT(portion: makePortion(resourceURL: nil), fileManager: fileManager)
+        
+        sut.deletePortionMedia()
+        
+        XCTAssertTrue(fileManager.loggedURLsForDeletion.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(portion: Portion, 
+                         fileManager: FileManageable = FileManagerSpy(),
+                         mediaSaver: MediaSaver = MediaSaverSpy()) -> StoryPortionViewModel {
+        let sut = StoryPortionViewModel(
+            story: makeStory(),
+            portion: portion,
+            fileManager: fileManager,
+            mediaSaver: mediaSaver
+        )
+        return sut
+    }
+}
+
+final class FileManagerSpy: FileManageable {
+    private(set) var loggedURLsForDeletion = [URL]()
+    
+    func saveImage(_ image: UIImage, fileName: String) throws -> URL {
+        anyImageURL()
+    }
+    
+    func getImage(for url: URL) -> UIImage? {
+        nil
+    }
+    
+    func delete(for url: URL) throws {
+        loggedURLsForDeletion.append(url)
+    }
+}
+
+final class MediaSaverSpy: MediaSaver {
+    func saveImageData(_ data: Data) async throws {
+        
+    }
+    
+    func saveVideo(by url: URL) async throws {
+        
+    }
+}
