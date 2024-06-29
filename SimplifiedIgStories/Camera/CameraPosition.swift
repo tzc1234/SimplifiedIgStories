1b8ffbe0d3db54d5358f907e89fbf4e2aabaa70b
tzc1234 <peopli9997@gmail.com>
2024-06-29 13:31:19 +0100

    Move CameraPosition to its own file

---
+++ SimplifiedIgStories/Camera/CameraPosition.swift
@@ -0,0 +1,13 @@
+//
+//  CameraPosition.swift
+//  SimplifiedIgStories
+//
+//  Created by Tsz-Lung on 29/06/2024.
+//
+
+import Foundation
+
+enum CameraPosition {
+    case back
+    case front
+}
