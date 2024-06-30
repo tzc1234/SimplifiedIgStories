5e9cdff3d7327de7b8d222ffe9b1633e2bb1c4de
tzc1234 <peopli9997@gmail.com>
2024-06-30 17:01:30 +0100

    Clean up StoriesAnimationHandlerTests

--- SimpleIGStoryTests/ViewModels/StoriesAnimationHandlerTests.swift
+++ SimpleIGStoryTests/ViewModels/StoriesAnimationHandlerTests.swift
@@ -24,10 +24,12 @@ final class StoriesAnimationHandlerTests: XCTestCase {
     }
     
     func test_currentStories_containsOnlyOneCurrentUserStoryWhenCurrentStoryIdIsCurrentUserStoryId() throws {
-        let stories = [makeStory(portions: [makePortion(id: 0)], isCurrentUser: true)]
+        let currentUserStoryId = 0
+        let stories = [makeStory(id: currentUserStoryId, portions: [makePortion(id: 0)], isCurrentUser: true)]
         let sut = makeSUT(stories: stories)
         
-        let currentUserStoryId = try XCTUnwrap(sut.stories.first { $0.user.isCurrentUser }?.id)
+        XCTAssertEqual(sut.currentStoryId, -1)
+        
         sut.setCurrentStoryId(currentUserStoryId)
         
         XCTAssertEqual(sut.currentStoryId, currentUserStoryId)
@@ -35,19 +37,19 @@ final class StoriesAnimationHandlerTests: XCTestCase {
         XCTAssertEqual(sut.currentStories.first?.id, currentUserStoryId)
     }
     
-    func test_currentStories_containsNoCurrentUserStoryWhenCurrentStoryIdIsNonCurrentUserStoryId() throws {
+    func test_currentStories_containsNoCurrentUserStoriesWhenCurrentStoryIdIsNonCurrentUserStoryId() throws {
+        let nonCurrentUserStoryId = 1
         let stories = [
             makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
-            makeStory(id: 1, portions: [makePortion(id: 1)])
+            makeStory(id: nonCurrentUserStoryId, portions: [makePortion(id: 1)])
         ]
         let sut = makeSUT(stories: stories)
         
-        let nonCurrentUserStoryId = try XCTUnwrap(sut.stories.first { !$0.user.isCurrentUser }?.id)
         sut.setCurrentStoryId(nonCurrentUserStoryId)
         
         XCTAssertEqual(sut.currentStoryId, nonCurrentUserStoryId)
-        let currentUserStoriesInCurrentStories = sut.currentStories.filter { $0.user.isCurrentUser }
-        XCTAssertEqual(currentUserStoriesInCurrentStories.count, 0)
+        let currentUserStories = sut.currentStories.filter { $0.user.isCurrentUser }
+        XCTAssertEqual(currentUserStories.count, 0)
     }
     
     func test_saveStoryIdBeforeDragged_savesCurrentStoryId() {
@@ -72,51 +74,52 @@ final class StoriesAnimationHandlerTests: XCTestCase {
     }
     
     func test_currentStoryIndex_deliversStoryIndexCorrectly() {
+        let currentUserStoryId = 0
+        let nonCurrentUserStoryId1 = 1
+        let nonCurrentUserStoryId2 = 2
         let stories = [
-            makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
-            makeStory(id: 1, portions: [makePortion(id: 1)]),
-            makeStory(id: 2, portions: [makePortion(id: 2)])
+            makeStory(id: currentUserStoryId, portions: [makePortion(id: 0)], isCurrentUser: true),
+            makeStory(id: nonCurrentUserStoryId1, portions: [makePortion(id: 1)]),
+            makeStory(id: nonCurrentUserStoryId2, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
         
-        let currentUserStoryId = 0
         sut.setCurrentStoryId(currentUserStoryId)
         
         XCTAssertEqual(sut.currentStoryIndex, 0)
         
-        let nonCurrentUserStoryId1 = 1
         sut.setCurrentStoryId(nonCurrentUserStoryId1)
         
         XCTAssertEqual(sut.currentStoryIndex, 0)
         
-        let nonCurrentUserStoryId2 = 2
         sut.setCurrentStoryId(nonCurrentUserStoryId2)
         
         XCTAssertEqual(sut.currentStoryIndex, 1)
     }
     
     func test_firstCurrentStoryId_deliversFirstStoryIdWhenItIsNotCurrentUserStory() {
+        let nonCurrentUserStoryId1 = 1
+        let nonCurrentUserStoryId2 = 2
         let stories = [
             makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
-            makeStory(id: 1, portions: [makePortion(id: 1)]),
-            makeStory(id: 2, portions: [makePortion(id: 2)])
+            makeStory(id: nonCurrentUserStoryId1, portions: [makePortion(id: 1)]),
+            makeStory(id: nonCurrentUserStoryId2, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
-        let nonCurrentUserStoryId = 2
         
-        sut.setCurrentStoryId(nonCurrentUserStoryId)
+        sut.setCurrentStoryId(nonCurrentUserStoryId2)
         
-        XCTAssertEqual(sut.firstCurrentStoryId, 1)
+        XCTAssertEqual(sut.firstCurrentStoryId, nonCurrentUserStoryId1)
     }
     
     func test_firstCurrentStoryId_deliversCurrentUserStoryIdWhenItIsCurrentUserStory() {
+        let currentUserStoryId = 0
         let stories = [
-            makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
+            makeStory(id: currentUserStoryId, portions: [makePortion(id: 0)], isCurrentUser: true),
             makeStory(id: 1, portions: [makePortion(id: 1)]),
             makeStory(id: 2, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
-        let currentUserStoryId = 0
         
         sut.setCurrentStoryId(currentUserStoryId)
         
@@ -124,27 +127,28 @@ final class StoriesAnimationHandlerTests: XCTestCase {
     }
     
     func test_lastCurrentStoryId_deliversLastStoryIdWhenItIsNotCurrentUserStory() {
+        let nonCurrentUserStoryId1 = 1
+        let nonCurrentUserStoryId2 = 2
         let stories = [
             makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
-            makeStory(id: 1, portions: [makePortion(id: 1)]),
-            makeStory(id: 2, portions: [makePortion(id: 2)])
+            makeStory(id: nonCurrentUserStoryId1, portions: [makePortion(id: 1)]),
+            makeStory(id: nonCurrentUserStoryId2, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
-        let nonCurrentUserStoryId = 1
         
-        sut.setCurrentStoryId(nonCurrentUserStoryId)
+        sut.setCurrentStoryId(nonCurrentUserStoryId1)
         
-        XCTAssertEqual(sut.lastCurrentStoryId, 2)
+        XCTAssertEqual(sut.lastCurrentStoryId, nonCurrentUserStoryId2)
     }
     
     func test_lastCurrentStoryId_deliversCurrentUserStoryIdWhenItIsCurrentUserStory() {
+        let currentUserStoryId = 0
         let stories = [
-            makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
+            makeStory(id: currentUserStoryId, portions: [makePortion(id: 0)], isCurrentUser: true),
             makeStory(id: 1, portions: [makePortion(id: 1)]),
             makeStory(id: 2, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
-        let currentUserStoryId = 0
         
         sut.setCurrentStoryId(currentUserStoryId)
         
@@ -152,66 +156,66 @@ final class StoriesAnimationHandlerTests: XCTestCase {
     }
     
     func test_isAtFirstStory_deliversTrueWhenCurrentStoryIsTheFirstCurrentOne() {
+        let currentUserStoryId = 0
+        let nonCurrentUserStoryId = 1
         let stories = [
-            makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
-            makeStory(id: 1, portions: [makePortion(id: 1)]),
+            makeStory(id: currentUserStoryId, portions: [makePortion(id: 0)], isCurrentUser: true),
+            makeStory(id: nonCurrentUserStoryId, portions: [makePortion(id: 1)]),
             makeStory(id: 2, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
-        let currentUserStoryId = 0
         
         sut.setCurrentStoryId(currentUserStoryId)
         
         XCTAssertTrue(sut.isAtFirstStory, "The Current user story is at the first")
         
-        let nonCurrentUserStoryId = 1
         sut.setCurrentStoryId(nonCurrentUserStoryId)
         
         XCTAssertTrue(sut.isAtFirstStory, "The 1st non-current user story is at the first")
     }
     
     func test_isAtFirstStory_deliversFalseWhenCurrentStoryIsNotTheFirstCurrentOne() {
+        let nonCurrentUserStoryId = 2
         let stories = [
             makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
             makeStory(id: 1, portions: [makePortion(id: 1)]),
-            makeStory(id: 2, portions: [makePortion(id: 2)])
+            makeStory(id: nonCurrentUserStoryId, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
         
-        let nonCurrentUserStoryId = 2
         sut.setCurrentStoryId(nonCurrentUserStoryId)
         
         XCTAssertFalse(sut.isAtFirstStory)
     }
     
     func test_isAtLastStory_deliversTrueWhenCurrentStoryIsTheLastCurrentOne() {
+        let currentUserStoryId = 0
+        let nonCurrentUserStoryId = 2
         let stories = [
-            makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
+            makeStory(id: currentUserStoryId, portions: [makePortion(id: 0)], isCurrentUser: true),
             makeStory(id: 1, portions: [makePortion(id: 1)]),
-            makeStory(id: 2, portions: [makePortion(id: 2)])
+            makeStory(id: nonCurrentUserStoryId, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
-        let currentUserStoryId = 0
         
         sut.setCurrentStoryId(currentUserStoryId)
         
         XCTAssertTrue(sut.isAtLastStory, "The Current user story is at the last")
         
-        let nonCurrentUserStoryId = 2
         sut.setCurrentStoryId(nonCurrentUserStoryId)
         
         XCTAssertTrue(sut.isAtLastStory, "The 2nd non-current user story is at the last")
     }
     
     func test_isAtLastStory_deliversFalseWhenCurrentStoryIsNotTheLastCurrentOne() {
+        let nonCurrentUserStoryId = 1
         let stories = [
             makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
-            makeStory(id: 1, portions: [makePortion(id: 1)]),
+            makeStory(id: nonCurrentUserStoryId, portions: [makePortion(id: 1)]),
             makeStory(id: 2, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
         
-        let nonCurrentUserStoryId = 1
         sut.setCurrentStoryId(nonCurrentUserStoryId)
         
         XCTAssertFalse(sut.isAtLastStory)
@@ -236,41 +240,43 @@ final class StoriesAnimationHandlerTests: XCTestCase {
     }
     
     func test_moveToPreviousStory_setsToCorrectStoryId() {
+        let previousStoryId = 1
+        let hasPreviousStoryId = 2
         let stories = [
             makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
-            makeStory(id: 1, portions: [makePortion(id: 1)]),
-            makeStory(id: 2, portions: [makePortion(id: 2)])
+            makeStory(id: previousStoryId, portions: [makePortion(id: 1)]),
+            makeStory(id: hasPreviousStoryId, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
-        let hasPreviousStoryId = 2
+        
         sut.setCurrentStoryId(hasPreviousStoryId)
+        sut.moveToPreviousStory()
+        
+        XCTAssertEqual(sut.currentStoryId, previousStoryId, "Moves to previous story after moveToPreviousStory called")
         
         sut.moveToPreviousStory()
         
-        XCTAssertEqual(sut.currentStoryId, 1, "Moves to previous story after moveToPreviousStory called")
-        
-        sut.moveToPreviousStory()
-        
-        XCTAssertEqual(sut.currentStoryId, 1, "Ignores when no previous story (exclude current user story)")
+        XCTAssertEqual(sut.currentStoryId, previousStoryId, "Ignores when no previous story (exclude current user story)")
     }
     
     func test_moveToNextStory_setsToCorrectStoryId() {
+        let hasNextStoryId = 1
+        let nextStoryId = 2
         let stories = [
             makeStory(id: 0, portions: [makePortion(id: 0)], isCurrentUser: true),
-            makeStory(id: 1, portions: [makePortion(id: 1)]),
-            makeStory(id: 2, portions: [makePortion(id: 2)])
+            makeStory(id: hasNextStoryId, portions: [makePortion(id: 1)]),
+            makeStory(id: nextStoryId, portions: [makePortion(id: 2)])
         ]
         let sut = makeSUT(stories: stories)
-        let hasNextStoryId = 1
+        
         sut.setCurrentStoryId(hasNextStoryId)
+        sut.moveToNextStory()
+        
+        XCTAssertEqual(sut.currentStoryId, nextStoryId, "Moves to next story after moveToNextStory called")
         
         sut.moveToNextStory()
         
-        XCTAssertEqual(sut.currentStoryId, 2, "Moves to next story after moveToNextStory called")
-        
-        sut.moveToNextStory()
-        
-        XCTAssertEqual(sut.currentStoryId, 2, "Ignores when no next story")
+        XCTAssertEqual(sut.currentStoryId, nextStoryId, "Ignores when no next story")
     }
     
     func test_getPortionCount_deliversZeroWithInvalidStoryId() {
@@ -280,27 +286,30 @@ final class StoriesAnimationHandlerTests: XCTestCase {
         ]
         let sut = makeSUT(stories: stories)
         
-        let receivedPortionCount = sut.getPortionCount(by: 2)
+        let invalidStoryId = 999
+        let receivedPortionCount = sut.getPortionCount(by: invalidStoryId)
         
         XCTAssertEqual(receivedPortionCount, 0)
     }
     
     func test_getPortionCount_deliversPortionCountCorrectly() {
-        let expectedPortions0 = [makePortion(id: 0)]
-        let expectedPortions1 = [makePortion(id: 1), makePortion(id: 2)]
+        let firstStoryId = 0
+        let secondStoryId = 1
+        let firstStoryPortions = [makePortion(id: 0)]
+        let secondStoryPortions = [makePortion(id: 1), makePortion(id: 2)]
         let stories = [
-            makeStory(id: 0, portions: expectedPortions0, isCurrentUser: true),
-            makeStory(id: 1, portions: expectedPortions1)
+            makeStory(id: firstStoryId, portions: firstStoryPortions, isCurrentUser: true),
+            makeStory(id: secondStoryId, portions: secondStoryPortions)
         ]
         let sut = makeSUT(stories: stories)
         
-        let receivedPortionCount0 = sut.getPortionCount(by: 0)
+        let firstReceivedPortionCount = sut.getPortionCount(by: firstStoryId)
         
-        XCTAssertEqual(receivedPortionCount0, expectedPortions0.count)
+        XCTAssertEqual(firstReceivedPortionCount, firstStoryPortions.count)
         
-        let receivedPortionCount1 = sut.getPortionCount(by: 1)
+        let secondReceivedPortionCount = sut.getPortionCount(by: secondStoryId)
         
-        XCTAssertEqual(receivedPortionCount1, expectedPortions1.count)
+        XCTAssertEqual(secondReceivedPortionCount, secondStoryPortions.count)
     }
     
     func test_subscribeObjectWillChange_triggersSelfObjectWillChangeWhenStoriesHolderObjectWillChangeGetTriggered() {
